099ecc41abe16f28611a7a68f36cc523682c860b
Jan Špaňhel <spainhell@gmail.com>
2021-07-25 17:20:05 +0200

    added more parts into key shortcut list

--- src/App.svelte
+++ src/App.svelte
@@ -2,42 +2,213 @@
 	import Keyb from './Keyb.svelte';
 </script>
 
-<h2>Datový editor</h2>
-<table>
-    <tr><td><Keyb labels={["&larr;"]}/></td><td>předchozí údaj</td></tr>
-    <tr><td><Keyb labels={["&rarr;"]}/></td><td>následující údaj</td></tr>
-    <tr><td><Keyb labels={["Enter"]}/></td><td>další editovaný údaj</td></tr>
-    <tr><td><Keyb labels={["Home"]}/></td><td>první údaj ve větě</td></tr>
-    <tr><td><Keyb labels={["End"]}/></td><td>poslední pořízený údaj ve větě</td></tr>
-    <tr><td><Keyb labels={["Ctrl", "&bsol;"]} delims={["+"]}/></td><td>na začátek další věty</td></tr>
-</table>
+<h1>PC FAND&reg</h1>
+<h2>Referenční seznam funkčních kláves, příkazů a funkcí</h2>
 
-<h2>Pohyb po větách</h2>
-<table>
-    <tr><td><Keyb labels={["Page Up"]}/></td><td>o obrazovku vzad</td></tr>
-    <tr><td><Keyb labels={["Page Down"]}/></td><td>o obrazovku vpřed</td></tr>
-    <tr><td><Keyb labels={["&uarr;", "Ctrl", "Home"]} delims={[", ", "+"]}/></td><td>předchozí věta</td></tr>
-    <tr><td><Keyb labels={["&darr;", "Ctrl", "End"]} delims={[", ", "+"]}/></td><td>následující věta</td></tr>
-    <tr><td><Keyb labels={["Ctrl", "Page Up"]} delims={["+"]}/></td><td>první věta</td></tr>
-    <tr><td><Keyb labels={["Ctrl", "Page Down"]} delims={["+"]}/></td><td>poslední věta</td></tr>
-    <tr><td><Keyb labels={["F3"]}/></td><td>najdi větu (podle klíč. údaje)</td></tr>
-    <tr><td><Keyb labels={["Ctrl", "F3"]} delims={["+"]}/></td><td>najdi větu (podle jejího čísla)</td></tr>
-</table>
+<ul>
+    <li>
+        <h3>I. Uživatelská část</h3>
+
+        <h3>Datový editor</h3>
+
+        <h4>Pohyb po údajích</h4>
+        <table>
+            <tr><td><Keyb labels={["&larr;"]}/></td><td>předchozí údaj</td></tr>
+            <tr><td><Keyb labels={["&rarr;"]}/></td><td>následující údaj</td></tr>
+            <tr><td><Keyb labels={["Enter"]}/></td><td>další editovaný údaj</td></tr>
+            <tr><td><Keyb labels={["Home"]}/></td><td>první údaj ve větě</td></tr>
+            <tr><td><Keyb labels={["End"]}/></td><td>poslední pořízený údaj ve větě</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "&bsol;"]} delims={["+"]}/></td><td>na začátek další věty</td></tr>
+        </table>
+
+        <h4>Pohyb po větách</h4>
+        <table>
+            <tr><td><Keyb labels={["Page Up"]}/></td><td>o obrazovku vzad</td></tr>
+            <tr><td><Keyb labels={["Page Down"]}/></td><td>o obrazovku vpřed</td></tr>
+            <tr><td><Keyb labels={["&uarr;", "Ctrl", "Home"]} delims={[", ", "+"]}/></td><td>předchozí věta</td></tr>
+            <tr><td><Keyb labels={["&darr;", "Ctrl", "End"]} delims={[", ", "+"]}/></td><td>následující věta</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Page Up"]} delims={["+"]}/></td><td>první věta</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Page Down"]} delims={["+"]}/></td><td>poslední věta</td></tr>
+            <tr><td><Keyb labels={["F3"]}/></td><td>najdi větu (podle klíč. údaje)</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F3"]} delims={["+"]}/></td><td>najdi větu (podle jejího čísla)</td></tr>
+        </table>
+
+        <h4>Editace údajů</h4>
+        <table>
+            <tr><td><Keyb labels={["Insert"]}/></td><td>zahájení opravy údaje</td></tr>
+            <tr><td><Keyb labels={["Insert", "Ctrl", "V"]} delims={[", ", "+"]}/></td><td>vkládání / přepis</td></tr>
+            <tr><td><Keyb labels={["&larr;"]}/></td><td>o znak doleva</td></tr>
+            <tr><td><Keyb labels={["&rarr;"]}/></td><td>o znak doprava</td></tr>
+            <tr><td><Keyb labels={["Home"]}/></td><td>na začátek údaje</td></tr>
+            <tr><td><Keyb labels={["End"]}/></td><td>na konec údaje</td></tr>
+            <tr><td><Keyb labels={["Delete"]}/></td><td>výmaz znaku nad kurzorem</td></tr>
+            <tr><td><Keyb labels={["Back space"]}/></td><td>výmaz znaku před kurzorem</td></tr>
+            <tr><td><Keyb labels={["Enter"]}/></td><td>ukončení editace údaje</td></tr>
+            <tr><td><Keyb labels={["Esc"]}/></td><td>-''-, ale bez provedení změn</td></tr>
+            <tr><td><Keyb labels={["F4"]}/></td><td>doplň diakritické znaménko</td></tr>
+            <tr><td><Keyb labels={["Alt", "F8"]} delims={["+"]}/></td><td>jiná klávesnice</td></tr>
+        </table>
+
+        <h4>Volné texty</h4>
+        <table>
+            <tr><td>'.'</td><td>prázdný text</td></tr>
+            <tr><td>'*'</td><td>text obsahuje znaky</td></tr>
+            <tr><td><Keyb labels={["Insert"]}/></td><td>editace</td></tr>
+            <tr><td><Keyb labels={["Esc"]}/></td><td>ukončení editace</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Home"]} delims={["+"]}/></td><td>předchozí volný text</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "End"]} delims={["+"]}/></td><td>následující volný text</td></tr>
+        </table>
+
+        <h4>Editace vět</h4>
+        <table>
+            <tr><td><Keyb labels={["F2"]}/></td><td>pořiď / edituj větu</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Y"]} delims={["+"]}/></td><td>vymaž větu (vybranou podmnožinu)</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "N"]} delims={["+"]}/></td><td>vlož novou větu před aktuální</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "U"]} delims={["+"]}/></td><td>obnov obsah věty před změnou</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "&larr;"]} delims={["+"]}/></td><td>přesuň větu o jednu nahoru</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "&rarr;"]} delims={["+"]}/></td><td>přesuň větu o jednu dolů</td></tr>
+            <tr><td><Keyb labels={["F4"]}/></td><td>duplikuj údaj (z přechozí věty)</td></tr>
+            <tr><td><Keyb labels={["F9"]}/></td><td>ulož</td></tr>
+            <tr><td><Keyb labels={["Esc"]}/></td><td>uložení a ukončení editace</td></tr>
+        </table>
+
+        <h4>Další klávesy</h4>
+        <table>
+            <tr><td><Keyb labels={["Ctrl", "F2"]} delims={["+"]}/></td><td>obnovení obrazovky</td></tr>
+            <tr><td><Keyb labels={["F5"]}/></td><td>PŘEPÍNAČE vypni/zapni</td></tr>
+            <tr><td>&nbsp;&nbsp;Podmnožina</td><td>výběr vět</td></tr>
+            <tr><td>&nbsp;&nbsp;Duplikace</td><td>kopírování údaje</td></tr>
+            <tr><td>&nbsp;&nbsp;Tabelátor</td><td>výběr údajů pro editaci</td></tr>
+            <tr><td>&nbsp;&nbsp;Aditivní změny</td><td>vazby do nadřízených souborů</td></tr>
+            <tr><td>&nbsp;&nbsp;Kontroly</td><td>kontroly zadaných hodnot</td></tr>
+            <tr><td><Keyb labels={["F6"]}/></td><td>AKCE</td></tr>
+            <tr><td>&nbsp;&nbsp;Opis</td><td>automatické tiskové sestavy</td></tr>
+            <tr><td>&nbsp;&nbsp;Kontroly</td><td>provedení kontrol (od akt. kurzoru)</td></tr>
+            <tr><td>&nbsp;&nbsp;Nová podmnožina</td><td>podmínka podmnožiny vět</td></tr>
+            <tr><td>&nbsp;&nbsp;Graf</td><td>grafické zobrazení dat</td></tr>
+            <tr><td>&nbsp;&nbsp;Třídit</td><td>setřídění souboru (neindexových)</td></tr>
+        </table>
+
+        <h4>Navigace po databázi</h4>
+        <table>
+            <tr><td><Keyb labels={["F7"]}/></td><td>navigace nahoru (nadřízený soubor)</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F7"]} delims={["+"]}/></td><td>navigace dolů (podřízený soubor)</td></tr>
+            <tr><td><Keyb labels={["Alt", "F7"]} delims={["+"]}/></td><td>navigace po všech souborech</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F4"]} delims={["+"]}/></td><td>návrat s převzetím hodnoty údaje</td></tr>
+            <tr><td><Keyb labels={["Shift", "F7"]} delims={["+"]}/></td><td>navigace nahoru s aut. převzetím</td></tr>
+        </table>
+
+        <h3>Textový editor</h3>
+        <p>Pozn. Následující příkazy platí pro textový editor (vyvolaný z hl. menu PC FANDu) a editaci volného textu v datovém souboru.</p>
+
+        <h4>Pohyb kurzoru po textu</h4>
+        <table>
+            <tr><td><Keyb labels={["&larr;"]}/></td><td>předchozí znak</td></tr>
+            <tr><td><Keyb labels={["&rarr;"]}/></td><td>následující znak</td></tr>
+            <tr><td><Keyb labels={["&uarr;"]}/></td><td>předchozí řádek</td></tr>
+            <tr><td><Keyb labels={["&darr;"]}/></td><td>následující řádek</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "&larr;"]} delims={["+"]}/></td><td>předchozí slovo</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "&rarr;"]} delims={["+"]}/></td><td>následující slovo</td></tr>
+            <tr><td><Keyb labels={["Home"]}/></td><td>začátek řádku</td></tr>
+            <tr><td><Keyb labels={["End"]}/></td><td>konec řádku</td></tr>
+            <tr><td><Keyb labels={["Page Up"]}/></td><td>o obrazovku vzad</td></tr>
+            <tr><td><Keyb labels={["Page Down"]}/></td><td>o obrazovku vpřed</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Page Up"]} delims={["+"]}/></td><td>začátek textu</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Page Down"]} delims={["+"]}/></td><td>konec textu</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F3"]} delims={["+"]}/></td><td>zadaný řádek číslo ...</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Q", "E"]} delims={["+"]}/></td><td>první řádek obrazovky</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Q", "X"]} delims={["+"]}/></td><td>poslední řádek obrazovky</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "W"]} delims={["+"]}/></td><td>obrazovka o řádek dolů</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Z"]} delims={["+"]}/></td><td>obrazovka o řádek nahoru</td></tr>
+        </table>
+
+        <h4>Editace textu</h4>
+        <table>
+            <tr><td><Keyb labels={["Enter"]}/></td><td>nový řádek / odstavec</td></tr>
+            <tr><td><Keyb labels={["Delete"]}/></td><td>výmaz znaku na kurzorem</td></tr>
+            <tr><td><Keyb labels={["Back space"]}/></td><td>výmaz znaku před kurzorem</td></tr>
+            ...
+        </table>
+
+        <h4>Kreslení rámečků</h4>
+        <p>Poz. Při kreslení rámečku v kódové stránce 852 nelze napojovat jednoduchou čáru na dvojitou a naopak.</p>
+
+        <h4>Přepínače textového editoru</h4>
+
+        <h4>Bloky textu</h4>
+        <p>Pozn. Clipboard je pomocná paměť pro přenos textu</p>
+
+        <h4>Formátování textu</h4>
+
+        <h4>Vyhledávání a nahrazování</h4>
+
+
+        <h3>Datový a textový editor</h3>
+        <h4>Kalkulačka</h4>
+        <h4>Nápověda</h4>
+
+        <h3>Aritmetické funkce</h3>
+
+        <h3>Tisk textu</h3>
+        <h4>Řídící klávesy</h4>
+        <h4>Ovládání tiskárny</h4>
+        <h4>Tečkové příkazy</h4>
+        <h4>Typy písma</h4>
+
+        <h3>Grafika</h3>
+        <h4>Zobrazení</h4>
+        <h4>Tisk, export</h4>
+
+        <h3>Operátory</h3>
+        <h4>Logické operátory</h4>
+        <h4>Srovnávací operátory</h4>
+        <h4>Číselné operátory</h4>
+        <h4>Textové operátory</h4>
+        <h4>Priorita operátorů</h4>
+
+    </li>
+
+    <li>
+        <h3>II. Programátorská část</h3>
+
+        <h3>Projektantské prostředí</h3>
+        <h4>Ladění a heslování</h4>
+        <h4>Nápověda</h4>
+        <h4>Další</h4>
+
+        <h3>Kódy řídících a funčních kláves</h3>
+        <h4>Bez prefixu '\0'</h4>
+        <h4>S prefixem '\0'</h4>
+
+        <h3>Módy datového editoru</h3>
+
+        <h3>Kód přerušení</h3>
+        <h4>Ukončení, exit událostí</h4>
+        <h4>Ukončení, exit funkčními klávesami</h4>
+        <h4>Ukončení, exit kurzorovými klávesami</h4>
+
+        <h3>Tabulky Znaků</h3>
+        Kódová stránka 1. část - společná<br>
+        Kódová stránka č. 437 IBM - 2. část<br>
+        Kódová stránka Kamenických - 2. část<br>
+        Kódová stránka LATIN II - 2. část<br>
+
+        <h3>Kódy barev</h3>
+    </li>
+</ul>
 
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	table, th, td {
+		border: solid grey 1px;
+        border-collapse: collapse;
 	}
 
-	h1 {
-		color: #ff3e00;
+	h1, h2, h3, h4 {
+		/*color: #ff3e00;*/
 		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		/*font-size: 4em;*/
+		/*font-weight: 100;*/
 	}
 
 	@media (min-width: 640px) {
