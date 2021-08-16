72665954c1ef072496d7bcacd5d1681db7c7da50
Jan Špaňhel <spainhell@gmail.com>
2021-08-16 09:11:44 +0200

    added more parts into key shortcut list added part of color table

--- src/App.svelte
+++ src/App.svelte
@@ -395,19 +395,178 @@
 
         <h3>Projektantské prostředí</h3>
         <h4>Ladění a heslování</h4>
+        <table>
+            <tr><td><Keyb labels={["Ctrl", "F8"]} delims={["+"]}/></td><td>syntaktická kontrola úlohy</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F9"]} delims={["+"]}/></td><td>kontrola a spuštění jedné kapitoly</td></tr>
+            <tr><td><Keyb labels={["Alt", "F9"]} delims={["+"]}/></td><td>spuštění úlohy kapitolou MAIN</td></tr>
+            <tr><td><Keyb labels={["Shift", "F1"]} delims={["+"]}/></td><td>navigace po projektu</td></tr>
+            <tr><td><Keyb labels={["F10"]}/></td><td>návrat z navigace</td></tr>
+            <tr><td><Keyb labels={["Alt", "F3"]} delims={["+"]}/></td><td>editace katalogu úlohy</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F10"]} delims={["+"]}/></td><td>odladění (zaheslení) úlohy</td></tr>
+            <tr><td colspan="2">Odpověď na dotaz - Chcete úlohu zaheslovat (A/N)?</td></tr>
+            <tr><td>A</td><td>odladit a nenávratně zaheslovat</td></tr>
+            <tr><td>N</td><td>pouze odladit</td></tr>
+            <tr><td><Keyb labels={["Alt", "F10"]} delims={["+"]}/></td><td>odladit a nenávratně zaheslovat</td></tr>
+        </table>
+
         <h4>Nápověda</h4>
+        <table>
+            <tr><td><Keyb labels={["Alt", "F1"]} delims={["+"]}/></td><td>nápověda k syntaxi kapitoly</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F1"]} delims={["+"]}/></td><td>nápověda k syntaxi klíčového slova</td></tr>
+            <tr><td><Keyb labels={["Alt", "F10"]} delims={["+"]}/></td><td>poslední vyvolaná nápověda</td></tr>
+            <tr><td><Keyb labels={["Alt", "F2"]} delims={["+"]}/></td><td>vyvolání editace nápovědy úlohy</td></tr>
+            <tr><td>z úlohy</td><td>editace help-souboru</td></tr>
+            <tr><td>z dat. souboru</td><td>editace nápovědy k údaji</td></tr>
+            <tr><td>z menu</td><td>editace nápovědy k položce menu</td></tr>
+            <tr><td colspan="2">Pozn. Při neexistenci hesla je věta spříslušným heslem automaticky doplněna.</td></tr>
+            <tr><td><Keyb labels={["F3"]}/></td><td>hledání názvu při editaci helpu</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "Home"]} delims={["+"]}/></td><td>předchozí věta help-souboru</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "End"]} delims={["+"]}/></td><td>následující věta help-souboru</td></tr>
+            <tr><td><Keyb labels={["F6"]}/></td><td>tisk aktuální zobrazené nápovědy</td></tr>
+        </table>
+
         <h4>Další</h4>
+        <table>
+            <tr><td><Keyb labels={["Ctrl", "&larr;"]} delims={["+"]}/></td><td>přesun kapitoly nahoru</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "&rarr;"]} delims={["+"]}/></td><td>přesun kapitoly dolů</td></tr>
+            <tr><td><Keyb labels={["F6"]}/></td><td>výpis zdrojového textu úlohy (tisk)</td></tr>
+            <tr><td colspan="2">Další použitelné kombinace kláves jsou stejné jako u textového editoru, tj. práce s bloky, hledání, nahrazování atd.</td></tr>
+        </table>
 
         <h3>Kódy řídících a funčních kláves</h3>
+        <p>Pozn. Použít tyto kódy lze ve spojitosti s funkcí SetKeyBuf();</p>
         <h4>Bez prefixu '\0'</h4>
+        <table>
+            <tr>
+                <td><Keyb labels={["Enter"]}/></td><td>\13</td>
+                <td><Keyb labels={["Tab"]}/></td><td>\9</td>
+            </tr>
+            <tr><td><Keyb labels={["Esc"]}/></td><td>\27</td><td></td><td></td></tr>
+            <tr><td><Keyb labels={["Ctrl", "P", "A"]} delims={["+"]}/> - <Keyb labels={["Ctrl", "P", "Z"]} delims={["+"]}/></td><td>\1 - \26</td><td></td><td></td></tr>
+        </table>
+
         <h4>S prefixem '\0'</h4>
+        <table>
+            <tr><td><Keyb labels={["Insert"]}/></td><td>\0\82</td>
+                <td><Keyb labels={["Ctrl", "="]} delims={["+"]}/></td><td>\0\131</td></tr>
+            <tr><td><Keyb labels={["Delete"]}/></td><td>\0\83</td>
+                <td><Keyb labels={["Ctrl", "Tab"]} delims={["+"]}/></td><td>\0\15</td></tr>
+            <tr><td><Keyb labels={["Home"]}/></td><td>\0\71</td>
+                <td><Keyb labels={["Ctrl", "Home"]} delims={["+"]}/></td><td>\0\119</td></tr>
+            <tr><td><Keyb labels={["End"]}/></td><td>\0\79</td>
+                <td><Keyb labels={["Ctrl", "End"]} delims={["+"]}/></td><td>\0\117</td></tr>
+            <tr><td><Keyb labels={["Page Up"]}/></td><td>\0\73</td>
+                <td><Keyb labels={["Ctrl", "Page Up"]} delims={["+"]}/></td><td>\0\132</td></tr>
+            <tr><td><Keyb labels={["Page Down"]}/></td><td>\0\81</td>
+                <td><Keyb labels={["Ctrl", "Page Down"]} delims={["+"]}/></td><td>\0\118</td></tr>
+            <tr><td><Keyb labels={["&larr;"]}/></td><td>\0\75</td>
+                <td><Keyb labels={["Ctrl", "&larr;"]} delims={["+"]}/></td><td>\0\115</td></tr>
+            <tr><td><Keyb labels={["&rarr;"]}/></td><td>\0\77</td>
+                <td><Keyb labels={["Shift", "&rarr;"]} delims={["+"]}/></td><td>\0\116</td></tr>
+            <tr><td><Keyb labels={["&uarr;"]}/></td><td>\0\72</td>
+                <td><Keyb labels={["&darr;"]}/></td><td>\0\80</td></tr>
+            <tr><td></td></tr>
+            <tr><td><Keyb labels={["F1"]}/></td><td>\0\59</td>
+                <td><Keyb labels={["Shift", "F1"]} delims={["+"]}/></td><td>\0\84</td></tr>
+            <tr><td><Keyb labels={["F2"]}/></td><td>\0\60</td>
+                <td><Keyb labels={["Shift", "F2"]} delims={["+"]}/></td><td>\0\85</td></tr>
+            <tr><td><Keyb labels={["F3"]}/></td><td>\0\61</td>
+                <td><Keyb labels={["Shift", "F3"]} delims={["+"]}/></td><td>\0\86</td></tr>
+            <tr><td><Keyb labels={["F4"]}/></td><td>\0\62</td>
+                <td><Keyb labels={["Shift", "F4"]} delims={["+"]}/></td><td>\0\87</td></tr>
+            <tr><td><Keyb labels={["F5"]}/></td><td>\0\63</td>
+                <td><Keyb labels={["Shift", "F5"]} delims={["+"]}/></td><td>\0\88</td></tr>
+            <tr><td><Keyb labels={["F6"]}/></td><td>\0\64</td>
+                <td><Keyb labels={["Shift", "F6"]} delims={["+"]}/></td><td>\0\89</td></tr>
+            <tr><td><Keyb labels={["F7"]}/></td><td>\0\65</td>
+                <td><Keyb labels={["Shift", "F7"]} delims={["+"]}/></td><td>\0\90</td></tr>
+            <tr><td><Keyb labels={["F8"]}/></td><td>\0\66</td>
+                <td><Keyb labels={["Shift", "F8"]} delims={["+"]}/></td><td>\0\91</td></tr>
+            <tr><td><Keyb labels={["F9"]}/></td><td>\0\67</td>
+                <td><Keyb labels={["Shift", "F9"]} delims={["+"]}/></td><td>\0\92</td></tr>
+            <tr><td><Keyb labels={["F10"]}/></td><td>\0\68</td>
+                <td><Keyb labels={["Shift", "F10"]} delims={["+"]}/></td><td>\0\93</td></tr>
+            <tr><td></td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F1"]} delims={["+"]}/></td><td>\0\94</td>
+                <td><Keyb labels={["Alt", "F1"]} delims={["+"]}/></td><td>\0\104</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F2"]} delims={["+"]}/></td><td>\0\95</td>
+                <td><Keyb labels={["Alt", "F2"]} delims={["+"]}/></td><td>\0\105</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F3"]} delims={["+"]}/></td><td>\0\96</td>
+                <td><Keyb labels={["Alt", "F3"]} delims={["+"]}/></td><td>\0\106</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F4"]} delims={["+"]}/></td><td>\0\97</td>
+                <td><Keyb labels={["Alt", "F4"]} delims={["+"]}/></td><td>\0\107</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F5"]} delims={["+"]}/></td><td>\0\98</td>
+                <td><Keyb labels={["Alt", "F5"]} delims={["+"]}/></td><td>\0\108</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F6"]} delims={["+"]}/></td><td>\0\99</td>
+                <td><Keyb labels={["Alt", "F6"]} delims={["+"]}/></td><td>\0\109</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F7"]} delims={["+"]}/></td><td>\0\100</td>
+                <td><Keyb labels={["Alt", "F7"]} delims={["+"]}/></td><td>\0\110</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F8"]} delims={["+"]}/></td><td>\0\101</td>
+                <td><Keyb labels={["Alt", "F8"]} delims={["+"]}/></td><td>\0\111</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F9"]} delims={["+"]}/></td><td>\0\102</td>
+                <td><Keyb labels={["Alt", "F9"]} delims={["+"]}/></td><td>\0\112</td></tr>
+            <tr><td><Keyb labels={["Ctrl", "F10"]} delims={["+"]}/></td><td>\0\103</td>
+                <td><Keyb labels={["Alt", "F10"]} delims={["+"]}/></td><td>\0\113</td></tr>
+        </table>
 
         <h3>Módy datového editoru</h3>
+        <table>
+            <tr><td>!!</td><td>editace pouze údajů dle parametru tab=, pokud není nastaven tab, nelze rušit a vkládat věty</td></tr>
+            <tr><td>^y</td><td>je zakázano zrušení vět</td></tr>
+            <tr><td>?y</td><td>před zrušením věty je vydán dotaz na potvrzení</td></tr>
+            <tr><td>^n</td><td>zákaz vytváření nových vět</td></tr>
+            <tr><td>F2</td><td>pořízení nových vět za koncem souboru a editace pouze těchto vět, mode='F2!!' - zákaz změn již pořízených vět</td></tr>
+            <tr><td>01</td><td>editace pouze jedné věty, mode='01F2' - pořízení pouze jedné věty</td></tr>
+            <tr><td>F3</td><td>hledání podle klíče, pokud věta dle zadaného klíče neexistuje, přepne se do režimu pořízení, po stisku <Keyb labels={["Esc"]}/> se hledání opakuje</td></tr>
+            <tr><td>LI</td><td>při hledání věty pomocí <Keyb labels={["F3"]}/> je pro údaje typu A a N kurzor aktuálně nastaven již v průběhu zadávání hodnoty klíče</td></tr>
+            <tr><td>&lt;=</td><td>potlačení hlášení "věta neexistuje" při hledání <Keyb labels={["F3"]}/></td></tr>
+            <tr><td>#A</td><td>zákaz vypnutí aditivních změn uživatelem</td></tr>
+            <tr><td>#L</td><td>zákaz vypnutí logických kontrol uživatelem</td></tr>
+            <tr><td>??</td><td>ukončení editoru bez dotazu, při mode='01??' i bez stisku <Keyb labels={["Esc"]}/></td></tr>
+            <tr><td>?e</td><td>vyžádá dotaz na ukončení při <Keyb labels={["Esc"]}/> (opak mode='??')</td></tr>
+            <tr><td>F1</td><td>krátká nápověda na posledním řádku obrazovky k aktuálnímu údaji, pokud heslo existuje v help-souboru, pomocí <Keyb labels={["Ctrl", "F8"]} delims={["+"]}/> se vyvolá nápověda přes celou obrazovku</td></tr>
+            <tr><td>F124</td><td>jako F1, ale na předposledním řádku obrazovky</td></tr>
+            <tr><td>CO</td><td>jen v kombinaci s cond=(...), podmínka cond je pouze výběr podmnožiny, nebude kontrolována při aktualizaci (pořízení)</td></tr>
+            <tr><td>^m</td><td>dvojitý klik levé klávesy myši přepnut z <Keyb labels={["Insert"]}/> na <Keyb labels={["Enter"]}/> (neplatí při editaci údaje)</td></tr>
+            <tr><td>-&gt;</td><td>klávesy <Keyb labels={["&uarr;"]}/> a <Keyb labels={["&darr;"]}/> se chovají stejně jako <Keyb labels={["&larr;"]}/> a <Keyb labels={["&rarr;"]}/>, tj. pohyb po údajích</td></tr>
+            <tr><td>r2</td><td>na druhý řádek obrazovky je vypsán výraz výběru podmnožiny (standardně se nevypisuje)</td></tr>
+            <tr><td>EX</td><td>exit-procedury vyvolané klávesou lze vyvolat i z editace údaje typu T</td></tr>
+            <tr><td>WX</td><td>pokud není určena podmnožina (cond, owner) u sdíleného indexového souboru, vytvoří se pracovní index umožňující ostatním účastníkům rušit věty (pouze LAN)</td></tr>
+        </table>
 
-        <h3>Kód přerušení</h3>
+        <h3>Kód přerušení (EDBREAK)</h3>
         <h4>Ukončení, exit událostí</h4>
+        <table>
+            <tr><td>0</td><td>ukončení <Keyb labels={["Esc"]}/></td></tr>
+            <tr><td>11</td><td>při překročení času - watch (LAN)</td></tr>
+            <tr><td>12</td><td>přerušení přechodem přes údaj (seznam údajů)</td></tr>
+            <tr><td>13</td><td>při prázdné podmnožině a <b>mode='^n'</b></td></tr>
+            <tr><td>14</td><td>z vložené editace <Keyb labels={["Ctrl", "F4"]} delims={["+"]}/></td></tr>
+            <tr><td>15</td><td>ukončení <Keyb labels={["Esc"]}/> při blokování (LAN)</td></tr>
+            <tr><td>16</td><td>nová věta nebo změna <b>exit=(...:RECORD)</b></td></tr>
+            <tr><td>17</td><td>zrušení věty <Keyb labels={["Ctrl", "Y"]} delims={["+"]}/></td></tr>
+        </table>
+
         <h4>Ukončení, exit funkčními klávesami</h4>
+        <table>
+            <tr><td>1 &divide; 10</td><td><Keyb labels={["Shift", "F1"]} delims={["+"]}/> až <Keyb labels={["Shift", "F10"]} delims={["+"]}/></td></tr>
+            <tr><td>21 &divide; 30</td><td><Keyb labels={["F1"]}/> až <Keyb labels={["F10"]}/></td></tr>
+            <tr><td>31 &divide; 40</td><td><Keyb labels={["Ctrl", "F1"]} delims={["+"]}/> až <Keyb labels={["Ctrl", "F10"]} delims={["+"]}/></td></tr>
+            <tr><td>41 &divide; 50</td><td><Keyb labels={["Alt", "F1"]} delims={["+"]}/> až <Keyb labels={["Alt", "F10"]} delims={["+"]}/></td></tr>
+        </table>
+
         <h4>Ukončení, exit kurzorovými klávesami</h4>
+        <table>
+            <tr><td>77</td><td><Keyb labels={["Tab"]}/></td><td>78</td><td><Keyb labels={["Ctrl", "Tab"]} delims={["+"]}/></td></tr>
+            <tr><td>51</td><td><Keyb labels={["Home"]}/></td><td>75</td><td><Keyb labels={["Ctrl", "Home"]} delims={["+"]}/></td></tr>
+            <tr><td>59</td><td><Keyb labels={["End"]}/></td><td>73</td><td><Keyb labels={["Ctrl", "End"]} delims={["+"]}/></td></tr>
+            <tr><td>53</td><td><Keyb labels={["Page Up"]}/></td><td>76</td><td><Keyb labels={["Ctrl", "Page Up"]} delims={["+"]}/></td></tr>
+            <tr><td>61</td><td><Keyb labels={["Page Down"]}/></td><td>74</td><td><Keyb labels={["Ctrl", "Page Down"]} delims={["+"]}/></td></tr>
+            <tr><td>55</td><td><Keyb labels={["&larr;"]}/></td><td>71</td><td><Keyb labels={["Ctrl", "&larr;"]} delims={["+"]}/></td></tr>
+            <tr><td>57</td><td><Keyb labels={["&rarr;"]}/></td><td>72</td><td><Keyb labels={["Ctrl", "&rarr;"]} delims={["+"]}/></td></tr>
+            <tr><td>52</td><td><Keyb labels={["&uarr;"]}/></td><td>60</td><td><Keyb labels={["&darr;"]}/></td></tr>
+            <tr><td>62</td><td><Keyb labels={["Insert"]}/></td></tr>
+        </table>
 
         <h3>Tabulky Znaků</h3>
         Kódová stránka 1. část - společná<br>
@@ -416,6 +575,24 @@
         Kódová stránka LATIN II - 2. část<br>
 
         <h3>Kódy barev</h3>
+        <table>
+            <tr>
+                <th>text pozadí</th>
+                <th>černý<br>zv.jas</th>
+                <th>modrý<br>zv.jas</th>
+                <th>zelený<br>zv.jas</th>
+                <th>cyan<br>zv.jas</th>
+                <th>červený<br>zv.jas</th>
+                <th>fialový<br>zv.jas</th>
+                <th>hnědý<br>zv.jas</th>
+                <th>šedý<br>zv.jas</th>
+            </tr>
+            <tr>
+                <td>černé</td>
+                <td>000<br>008</td><td>001<br>009</td><td>002<br>010</td><td>003<br>011</td>
+                <td>004<br>012</td><td>005<br>013</td><td>006<br>014</td><td>007<br>015</td>
+            </tr>
+        </table>
     </li>
 </ul>
 
